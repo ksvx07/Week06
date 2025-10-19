fc6116fe15f67dd8091c9233357aa59bc0753058
Puryeo <forgamedesign03@gmail.com>
2025-10-19 23:14:53 +0900

    올려

---
+++ Assets/Scripts/KeyRigidBodyDestroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyRigidBodyDestroy : MonoBehaviour
+{
+    [SerializeField] private Button _exitBtn;
+    [SerializeField] private Rigidbody rb;
+
+    private void OnEnable()
+    {
+        _exitBtn.onClick.AddListener(ExitBtn);
+    }
+    
+    private void OnDisable()
+    {
+        _exitBtn.onClick.RemoveAllListeners();
+    }   
+
+    void ExitBtn()
+    {
+        rb.isKinematic = true;
+    }
+
+}
